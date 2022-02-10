668e822f691e08c338262f16f242b8e856e8f9b7
Safa Aru <101331910@georgebrown.ca>
2022-02-09 21:31:11 -0500

    Lab2-TipCalculator

---
+++ Lab2/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Lab2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Fifteen = 0;
+            ViewBag.Twenty = 0;
+            ViewBag.TwentyFive = 0;
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(TipCalculator tip)
+        {
+            if(ModelState.IsValid)
+            {
+                ViewBag.Fifteen = tip.CalculateTip(0.15);
+                ViewBag.Twenty = tip.CalculateTip(0.20);
+                ViewBag.TwentyFive = tip.CalculateTip(0.25);
+            }
+            else
+            {
+                ViewBag.Fifteen = 0;
+                ViewBag.Twenty = 0;
+                ViewBag.TwentyFive = 0;
+            }
+            return View(tip);
+        }
+    }
+}
---
+++ Lab2/Models/TipCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab2.Models
+{
+    public class TipCalculator
+    {
+        [Required(ErrorMessage= "Please enter a value for the cost of the meal")]
+        [Range(0.0, 1000.0, ErrorMessage="Cost of meal must be greater than $0, and less or equal to $1000")]
+
+        public double? MealCost { get; set; }
+
+        public double CalculateTip(double percent)
+        {
+            if (MealCost.HasValue)
+            {
+                var tip = MealCost.Value * percent;
+                return tip;
+            }
+            else
+            {
+                return 0.0;
+            }
+        }
+
+    }
+}
